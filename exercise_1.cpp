97b583b08b29ac567794cc00807ba6c679085cb0
Samuel Chamalé <63200593+chamale-rac@users.noreply.github.com>
2022-09-01 17:20:03 -0600

    Update exercise_1.cpp
    Codigo funcionando, se necesita optimización aún.

--- exercise_1.cpp
+++ exercise_1.cpp
@@ -1,7 +1,7 @@
 
 /*
 *------------------------------------------
-* exercise_1.cpp
+* ex1_remastered.cpp
 * -----------------------------------------
 * UNIVERSIDAD DEL VALLE DE GUATEMALA
 * FACULTAD DE INGENIERÍA
@@ -9,9 +9,9 @@
 * CC3086 - Programacion de Microprocesadores
 *
 * by Samuel Chamalé
-* jueves, 1 de septiembre del 2022
+* jueves, 25 de agosto del 2022
 *------------------------------------------
-* Description: ... 
+* Description: Basic program that multiplies prime numbers given an interval (Using threads).
 *------------------------------------------
 */
 
@@ -20,14 +20,13 @@
 #include <bits/stdc++.h>
 #include <string>
 using namespace std;
-#define monthsNumber 2;
-#define productsNumber 8;
+#define monthsNumber 2
+#define productsNumber 8
 
 std::string productNames[productsNumber] = {"Porcion Pastel Chocolate","White Mocha","Cafe americano","Latte","Toffee Coffee","Cappucino","S'mores Latte","Cafe tostado molido"};
-int unitPrices[productsNumber] = {60, 32, 22, 24, 28, 24, 32, 60};
+float unitPrices[productsNumber] = {60, 32, 22, 24, 28, 24, 32, 60};
 float staticCosts[productsNumber] = {20, 19.2, 13.2, 17.2, 20.1, 17.2, 23, 20};
-int unitsSold[monthsNumber][productsNumber] = {{300, 400, 1590, 200, 390, 1455, 800, 60}, {250, 380, 800, 250, 600, 1200, 1540, 15}};
-int index;
+float unitsSold[monthsNumber][productsNumber] = {{300, 400, 1590, 200, 390, 1455, 800, 60}, {250, 380, 800, 250, 600, 1200, 1540, 15}};
 
 void dataFiller();
 void operationsMonth();
@@ -41,7 +40,7 @@ struct productData {
     //Unknow
     float sold;
     float utility;
-}
+};
 
 struct monthData {
     //Know
@@ -51,35 +50,47 @@ struct monthData {
     float sold;
     float costs;
     float utilities;
-}
+};
+
 monthData *months;
 
 struct threadData {
     int month;
     int product;
-} 
-threadData thrData;
+};
+
+threadData *thrData;
 
 void *operationsProduct(void *arg) {
-    intervalData *ptr = (thrData*) arg;
-    productData *product = &months[ptr -> month].products[ptr -> product];
-
-    product.sold = product.price * product.soldUnits;  
-    product.utility = product.sold - (product.soldUnits * product.fixedCost); 
+    threadData *pointer = (threadData*) arg;
+    int i = pointer -> month;
+    int j = pointer -> product;
+    
+    //std::cout << i << std::endl; 
+    //std::cout << j << std::endl; 
+    
+    months[i].products[j].sold = months[i].products[j].price * months[i].products[j].soldUnits;
+    months[i].products[j].utility = months[i].products[j].sold - (months[i].products[j].soldUnits * months[i].products[j].fixedCost);
     return nullptr;
 }
 
 int main(int argc, char *argv[]) {
     dataFiller();
   
-    pthread_t threads[monthsNumber*productsNumber];
-
-    index = 0;
+    pthread_t threads[16];
+    
+    thrData = new threadData[16];
+    
+    int index = 0;
     for(int i = 0; i < monthsNumber; i++){
         for(int j = 0; j < productsNumber; j++){ 
-            thrData.month = i;
-            thrData.product = j;
-            pthread_create(&threads[index], nullptr, operationsProduct, &thrData);
+            thrData[index].month = i;
+            thrData[index].product = j;
+            //std::cout << thrData.month << std::endl; 
+            //std::cout << thrData.product << std::endl; 
+            std::cout << "\n" << std::endl; 
+            
+            pthread_create(&threads[index], nullptr, operationsProduct, &thrData[index]);
             index++;
         }
     }
@@ -114,6 +125,12 @@ void dataFiller() {
             months[i].products[j].price = unitPrices[j];
             months[i].products[j].fixedCost = staticCosts[j];
             months[i].products[j].soldUnits  = unitsSold[i][j];
+            
+            //std::cout << months[i].products[j].name << std::endl; 
+            //std::cout << months[i].products[j].price << std::endl; 
+            //std::cout << months[i].products[j].fixedCost << std::endl;
+            //std::cout << months[i].products[j].soldUnits << std::endl;
+            //std::cout << "\n" << std::endl; 
         }
     }        
 }
@@ -123,13 +140,17 @@ void operationsMonth() {
         for(int j = 0; j < productsNumber; j++){
             months[i].sold += months[i].products[j].sold;
             months[i].utilities += months[i].products[j].utility;
+            
+            //std::cout << months[i].products[j].name << std::endl; 
+            //std::cout << months[i].products[j].sold << std::endl; 
+            //std::cout << months[i].products[j].utility << std::endl; 
+            //std::cout << "\n" << std::endl; 
         }
         months[i].utilities = months[i].utilities - months[i].costs;
+        std::cout << months[i].sold << std::endl; 
+        std::cout << months[i].costs << std::endl; 
+        std::cout << months[i].utilities << std::endl;  
+        std::cout << "\n" << std::endl; 
+        
     }  
 }
-
-// REFERENCES
-// https://es.stackoverflow.com/questions/46909/cual-es-la-diferencia-entre-int-e-int
-// https://www.geeksforgeeks.org/array-of-strings-in-cpp-5-different-ways-to-create/
-// https://www.w3schools.com/cpp/cpp_structs.asp#:~:text=Structures%20(also%20called%20structs)%20are,%2C%20bool%2C%20etc.).
-// https://cplusplus.com/doc/tutorial/arrays/
